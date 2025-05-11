4ca1230db53f1cada5add68f4975f22a6cd1c7bd
adeleeeeyyyy <mitsuhaadly@gmail.com>
2025-05-11 20:35:35 +0700

    fix image

--- resources/views/profiles/public.blade.php
+++ resources/views/profiles/public.blade.php
@@ -11,12 +11,8 @@
 <body class="bg-[#c6ffc6]">
     <div class="p-8 rounded-lg">
         <div class="flex w-full flex-col items-center">
-            <div class="w-20 h-20 rounded-full overflow-hidden border-2 border-gray-300">
-                <img
-                    src="{{ $user->avatar ? url('storage/' . $user->avatar) : 'https://ui-avatars.com/api/?name=' . urlencode($user->name) }}"
-                    alt="{{ $user->name }}"
-                    class="w-full h-full object-cover"
-                >
+            <div class="flex items-center w-20 h-20 overflow-hidden">
+                <img src="{{ $user->avatar ? url('storage/' . $user->avatar) : 'https://ui-avatars.com/api/?name=' . $user->name }}" alt="{{ $user->name }}" class="w-full h-full object-center rounded-full border-2 object-cover">
             </div>
             <div>
                 <h2 class="text-xl font-bold">{{ $user->name }}</h2>
