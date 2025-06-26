7fdc55e6865ed783897c39133e6f81d2fddb0bf6
adeleeeeyyyy <mitsuhaadly@gmail.com>
2025-06-26 23:39:00 +0700

    bnerin route sikit

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,13 +12,13 @@
 
         <!-- Styles / Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
-       
+
     </head>
     <body class="md:w-[60%] md:p-20 flex-col p-4 h-screen relative flex items-center mx-auto bg-[#C6ffC6]">
         <div class="">
             <h1 class="md:text-4xl text-2xl font-bold mb-10">Login</h1>
         </div>
-        
+
         <div class="w-full h-fit bg-green-300 p-4 border-2 rounded-lg">
             <form action="/logins" method="POST" class="flex flex-col gap-5">
                 @csrf
@@ -29,6 +29,9 @@
                 <div class="mt-4">
                     <label for="password" class="block text-gray-700 text-sm font-bold mb-2">Password</label>
                     <input type="password" name="password" id="password" class="shadow-[5px_5px_0px_0px_rgba(0,0,0,255)] appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
+                </div>
+                <div>
+                    <p>Didn't have an account yet? <a href="/register" class="text-blue-600 underline font-bold">Register Here.</a></p>
                 </div>
                 <div class="mt-4">
                     <button type="submit" class="bg-green-400 shadow-[5px_5px_0px_0px_rgba(0,0,0,255)] border-2 hover:translate-y-[5px] hover:translate-x-[5px] hover:shadow-none transition-all font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::redirect('/', '/register');
+Route::redirect('/', '/login');
 Route::get('/{username}', [ProfileController::class, 'show'])
     ->where('username', '^(?!login|register|dashboard|profile).*$');
 
